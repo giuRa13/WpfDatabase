0adee4fd66771115b7ad0a20105605b99890b753
giuRa13 <giulioravazzolobass@gmail.com>
2025-02-03 19:25:53 +0100

    buttons, enable/disable

--- WpfDatabase/MainWindow.xaml.cs
+++ WpfDatabase/MainWindow.xaml.cs
@@ -29,13 +29,83 @@ namespace WpfDatabase
         {
 
             WpfDatabase.MainDs.EmployeesDs employeesDs = ((WpfDatabase.MainDs.EmployeesDs)(this.FindResource("employeesDs")));
-            // Load data into the table Employee_Info. You can modify this code as needed.
             WpfDatabase.MainDs.EmployeesDsTableAdapters.Employee_InfoTableAdapter employeesDsEmployee_InfoTableAdapter = new WpfDatabase.MainDs.EmployeesDsTableAdapters.Employee_InfoTableAdapter();
             employeesDsEmployee_InfoTableAdapter.Fill(employeesDs.Employee_Info);
+            
             System.Windows.Data.CollectionViewSource employee_InfoViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("employee_InfoViewSource")));
             employee_InfoViewSource.View.MoveCurrentToFirst();
+            this.employee_InfoDataGrid.ScrollIntoView(employee_InfoViewSource.View.CurrentItem);
 
+            this.save_btn.IsEnabled = false;
+            this.save_btn.Opacity = 0.5;
+            this.return_btn.IsEnabled = false;
+            this.return_btn.Opacity = 0.5;
+
+            this.group_box_left.IsEnabled = false;
+            this.group_box_left.Opacity = 0.5;
+            this.employee_InfoDataGrid.IsEnabled = true;
             
+        }
+
+        void new_edit_del_btns()
+        {
+            this.new_btn.IsEnabled = false;
+            this.new_btn.Opacity = 0.5;
+            this.edit_btn.IsEnabled = false;
+            this.edit_btn.Opacity = 0.5;
+
+            this.save_btn.IsEnabled = true;
+            this.save_btn.Opacity = 1.0;
+            this.return_btn.IsEnabled = true;
+            this.return_btn.Opacity = 1.0;
+
+            this.group_box_left.IsEnabled = true;
+            this.group_box_left.Opacity = 1.0;
+            this.employee_InfoDataGrid.IsEnabled = false;
+        }
+
+        void save_return_btns()
+        {
+            this.new_btn.IsEnabled = true;
+            this.new_btn.Opacity = 1.0;
+            this.edit_btn.IsEnabled = true;
+            this.edit_btn.Opacity = 1.0;
+            this.delete_btn.IsEnabled = true;
+            this.delete_btn.Opacity = 1.0;
+
+            this.save_btn.IsEnabled = false;
+            this.save_btn.Opacity = 0.5;
+            this.return_btn.IsEnabled = false;
+            this.return_btn.Opacity = 0.5;
+
+            this.group_box_left.IsEnabled = false;
+            this.group_box_left.Opacity = 0.5;
+            this.employee_InfoDataGrid.IsEnabled = true;
+        }
+
+        private void new_btn_Click(object sender, RoutedEventArgs e)
+        {
+            new_edit_del_btns();
+        }
+
+        private void edit_btn_Click(object sender, RoutedEventArgs e)
+        {
+            new_edit_del_btns();
+        }
+
+        private void delete_btn_Click(object sender, RoutedEventArgs e)
+        {
+            new_edit_del_btns();
+        }
+
+        private void save_btn_Click(object sender, RoutedEventArgs e)
+        {
+            save_return_btns();
+        }
+
+        private void return_btn_Click(object sender, RoutedEventArgs e)
+        {
+            save_return_btns();
         }
     }
 }
